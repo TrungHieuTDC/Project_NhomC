d2a480ede5228f50ec51119868c8c0f99c125a82
[TrungHieuTDC] <[21211TT0234@mail.tdc.edu.vn]>
2023-05-27 05:19:36 +0800

    source tong hop cua nhom

--- demo/database/migrations/2014_10_12_000000_create_users_table.php
+++ demo/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,29 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id();	
+            $table->integer('age')->nullable();	
+            $table->text('post')->nullable();	
+            $table->string('name',100);	
+            $table->string('email',100);
+            $table->string('sdt',100);	
+            $table->text('image');
+            $table->timestamp('email_verified_at')->nullable();	
+            $table->string('password',100);	
+            $table->rememberToken();	
+            $table->timestamps();	
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
